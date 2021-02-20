3eef5d520405dae1f770e77f6be20729f6a995f2
ujjwal mittal <ujjwalmittal07@gmail.com>
2021-02-21 04:23:27 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, redirect, request
 import gpt_2_simple as gpt
 
 app = Flask(__name__)
-sess = None
-model_loaded = False
+model_loaded= False
 
 @app.route('/')
 def index():
@@ -13,19 +12,21 @@ def index():
 def load():
     global sess
     global model_loaded
-    # sess = gpt.start_tf_sess()
-    # gpt.load_gpt2(sess)
     model_loaded = True
     return redirect('/')
+  
+
 
 @app.route('/generate', methods=['GET', 'POST'])
 def gen():
     if request.method == 'POST':
         dets = request.form
-        dream = dets['dream']
+        dream = str(dets['dream'])
         length = int(dets['length'])
-        res = dream, length
-        # res = gpt.generate(sess, length=length, prefix=dream, temperature=0.7, run_name='run1', include_prefix=False, nsamples=3, return_as_list=True)
+        sess = gpt.start_tf_sess()
+        gpt.load_gpt2(sess)
+        res= gpt.generate(sess, length = length,
+             run_name = 'run1', prefix =dream,return_as_list=True,nsamples= 3)
         return render_template('generate.html', text=res)
 
 if __name__ == '__main__':
